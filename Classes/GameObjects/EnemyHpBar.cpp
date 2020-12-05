04cd39208bf639a0e911fa2b02d0d919c680fd3f
Krolo <mateusz.krol.it@gmail.com>
2020-12-05 15:51:09 +0100

    enemy hp bar outline

--- Classes/GameObjects/EnemyHpBar.cpp
+++ Classes/GameObjects/EnemyHpBar.cpp
@@ -9,8 +9,10 @@ EnemyHpBar* EnemyHpBar::createEnemyHpBar()
 		return NULL;
 	}
 
-	ret->drawNode = cocos2d::DrawNode::create();
-	ret->addChild(ret->drawNode);
+	ret->bar = cocos2d::DrawNode::create();
+	ret->outline = cocos2d::DrawNode::create();
+	ret->addChild(ret->outline);
+	ret->addChild(ret->bar);
 	ret->setLocalZOrder(2);
 	return ret;
 }
@@ -20,12 +22,19 @@ void EnemyHpBar::updateHpBar(int hp)
 	auto dest = ENEMY_HP_BAR_POS;
 	dest.y += (float)ENEMY_HP_BAR_HEIGHT;
 	dest.x += (float)ENEMY_HP_BAR_MAX_WIDTH * (float)hp / (float)maxHp;
+
+	auto outlineDest = cocos2d::Vec2(dest.x + OUTLINE_THICKNESS, dest.y + OUTLINE_THICKNESS);
 	
 	auto origin = Settings::getTranslatedCoords(ENEMY_HP_BAR_POS);
 	dest = Settings::getTranslatedCoords(dest);
+
+	auto outlineOrigin = Settings::getTranslatedCoords(ENEMY_HP_BAR_OUTLINE_POS);
+	outlineDest = Settings::getTranslatedCoords(outlineDest);
 	
-	drawNode->clear();
-	drawNode->drawSolidRect(origin, dest, cocos2d::Color4F::WHITE);
+	bar->clear();
+	outline->clear();
+	bar->drawSolidRect(origin, dest, cocos2d::Color4F::WHITE);
+	outline->drawSolidRect(outlineOrigin, outlineDest, cocos2d::Color4F::BLACK);
 }
 
 void EnemyHpBar::setMaxHp(int newMax)
--- Classes/GameObjects/EnemyHpBar.h
+++ Classes/GameObjects/EnemyHpBar.h
@@ -13,10 +13,15 @@ public:
 
 private:
 	int maxHp = 0;
-	cocos2d::DrawNode* drawNode = nullptr;
+	cocos2d::DrawNode* bar = nullptr;
+	cocos2d::DrawNode* outline = nullptr;
 
-	const cocos2d::Vec2 ENEMY_HP_BAR_POS = cocos2d::Vec2(110, 878);
 	const int ENEMY_HP_BAR_MAX_WIDTH = 660;
 	const int ENEMY_HP_BAR_HEIGHT = 6;
+	const int OUTLINE_THICKNESS = 2;
+
+	const cocos2d::Vec2 ENEMY_HP_BAR_POS = cocos2d::Vec2(110, 878);
+	const cocos2d::Vec2 ENEMY_HP_BAR_OUTLINE_POS =
+		cocos2d::Vec2(110 - OUTLINE_THICKNESS, 878 - OUTLINE_THICKNESS);
 };
 
